61ea9736576443cb6734485b753302e061b93b13
Muhammedashraf10 <muhammedashraf192@gmail.com>
2023-11-18 14:40:21 +0200

    Create version.tf

---
+++ VPC/version.tf
@@ -0,0 +1,17 @@
+#Terraform Versions
+
+terraform {
+  required_version = "~> 1.6.3"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = "default"
+}
+
